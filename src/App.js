8270505a8d3be8b794c91b54a63b5f82b8f171f5
kamta <kamtap302@gmail.com>
2021-10-15 00:53:59 +0530

    sljfl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from "react";
+export default function App() {
+  let time = new Date().toLocaleTimeString();
+  const [ctime, setTime] = useState(time);
+  const cssStyle = {};
+
+
+  function update() {
+    time = new Date().toLocaleTimeString();
+
+    setTime(time);
+
+
+  }
+  setInterval(update, 1000);
+  var d = new Date();
+  var n = d.getSeconds();
+
+  // console.log(n)
+  if (n % 2 === 0) {
+    cssStyle.backgroundColor = 'black';
+    cssStyle.padding = "400px";
+
+
+  }
+  //
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1 >{ctime}</h1>
+      <div style={cssStyle}>
+
+      </div>
+
     </div>
+
   );
 }
-
-export default App;
